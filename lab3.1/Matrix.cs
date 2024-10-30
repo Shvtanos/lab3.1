834f68b4558e58e8270ec58759b28e22196d3298
Татьяна <@DESKTOP-ITD1IV1>
2024-10-30 20:10:03 +0500

    Добавьте файлы проекта.

---
+++ lab3.1/Matrix.cs
@@ -0,0 +1,213 @@
+﻿using System;
+
+namespace lab3._1
+{
+    internal class Matrix
+    {
+        private int[,] array;
+
+        // Конструктор для ввода массива с клавиатуры (задание 1)
+        public Matrix(int n, int m)
+        {
+            array = new int[n, m];
+            Console.WriteLine("Введите элементы массива построчно:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    array[i, j] = InputInteger($"Элемент [{i},{j}]: ");
+                }
+            }
+        }
+
+        // Конструктор для создания массива с шахматным заполнением случайными числами (задание 2)
+        public Matrix(int n, bool chessPattern)
+        {
+            array = new int[n, n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    bool isBlackCell = (i + j) % 2 == 0;
+                    int randomValue;
+                    do
+                    {
+                        randomValue = rand.Next(1, 101); // случайное число от 1 до 100
+                    } while (isBlackCell ? randomValue % 2 != 0 : randomValue % 2 == 0);
+
+                    array[i, j] = randomValue;
+                }
+            }
+        }
+
+        // Конструктор для создания массива с диагональным заполнением (задание 3)
+        public Matrix(int n)
+        {
+            array = new int[n, n];
+            FillDiagonalPattern(n);
+        }
+
+        // Метод для заполнения диагонального массива
+        private void FillDiagonalPattern(int n)
+        {
+            int number = 1;
+
+            // Заполняем массив по диагоналям, начиная с нижней левой части к верхней правой
+            for (int startRow = n - 1; startRow >= 0; startRow--)
+            {
+                int row = startRow;
+                int col = 0;
+                while (row < n && col < n)
+                {
+                    array[row, col] = number++;
+                    row++;
+                    col++;
+                }
+            }
+
+            // Продолжаем заполнение по диагоналям справа от главной диагонали
+            for (int startCol = 1; startCol < n; startCol++)
+            {
+                int row = 0;
+                int col = startCol;
+                while (row < n && col < n)
+                {
+                    array[row, col] = 0;
+                    row++;
+                    col++;
+                }
+            }
+        }
+
+        // Метод для вычисления сумм столбцов (задание 2)
+        public int[] GetColumnSums()
+        {
+            int cols = array.GetLength(1);
+            int[] columnSums = new int[cols - 1];
+
+            for (int j = 0; j < cols - 1; j++)
+            {
+                int sum = 0;
+                for (int i = 0; i < array.GetLength(0) - j - 1; i++)
+                {
+                    sum += array[i, j];
+                }
+                columnSums[j] = sum;
+            }
+            return columnSums;
+        }
+
+        // Метод для нахождения максимальной суммы из массива столбцовых сумм
+        public int MaxColumnSum()
+        {
+            int[] columnSums = GetColumnSums();
+            int maxSum = int.MinValue;
+            foreach (int sum in columnSums)
+            {
+                if (sum > maxSum)
+                    maxSum = sum;
+            }
+            return maxSum;
+        }
+
+        // Перегрузка оператора + для сложения двух массивов
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            int n = a.array.GetLength(0);
+            Matrix result = new Matrix(n);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result.array[i, j] = a.array[i, j] + b.array[i, j];
+                }
+            }
+            return result;
+        }
+
+        // Перегрузка оператора * для умножения массива на число
+        public static Matrix operator *(Matrix a, int scalar)
+        {
+            int n = a.array.GetLength(0);
+            Matrix result = new Matrix(n);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result.array[i, j] = a.array[i, j] * scalar;
+                }
+            }
+            return result;
+        }
+
+        // Перегрузка оператора - для вычитания массивов
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            int n = a.array.GetLength(0);
+            Matrix result = new Matrix(n);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result.array[i, j] = a.array[i, j] - b.array[i, j];
+                }
+            }
+            return result;
+        }
+
+        // Метод для транспонирования матрицы
+        public Matrix Transpose()
+        {
+            int n = array.GetLength(0);
+            Matrix transposed = new Matrix(n);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    transposed.array[j, i] = array[i, j];
+                }
+            }
+            return transposed;
+        }
+
+        // Перегрузка метода ToString для вывода массива в виде таблицы
+        public override string ToString()
+        {
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+            string result = "";
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result += array[i, j].ToString().PadLeft(3) + " ";
+                }
+                result += Environment.NewLine;
+            }
+            return result;
+        }
+
+        // Основной метод для выполнения выражения (A + 4 * B) - C^T
+        public static Matrix CalculateExpression(Matrix A, Matrix B, Matrix C)
+        {
+            return (A + B * 4) - C.Transpose();
+        }
+
+        // Вспомогательный метод для безопасного ввода целого числа
+        public static int InputInteger(string prompt)
+        {
+            int value;
+            while (true)
+            {
+                Console.Write(prompt);
+                if (int.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+                Console.WriteLine("Ошибка: введите целое число.");
+            }
+        }
+    }
+}
+
---
+++ lab3.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using lab3._1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задание 1: Ввод массива с клавиатуры
+        Console.WriteLine("1. Введите размерность массива (n x m):");
+        int n1 = Matrix.InputInteger("Введите количество строк (n): ");
+        int m1 = Matrix.InputInteger("Введите количество столбцов (m): ");
+
+        Matrix matrix1 = new Matrix(n1, m1);
+        Console.WriteLine("Массив, введённый с клавиатуры:");
+        Console.WriteLine(matrix1);
+
+        // Задание 1: Шахматное заполнение случайными числами
+        int n2 = Matrix.InputInteger("Введите размерность квадратного массива для шахматного заполнения (n x n): ");
+
+        Matrix chessMatrix = new Matrix(n2, true);
+        Console.WriteLine("Шахматный массив:");
+        Console.WriteLine(chessMatrix);
+
+        // Задание 1: Диагональное заполнение для произвольного n
+        int n3 = Matrix.InputInteger("Введите размерность квадратного массива для диагонального заполнения (n x n): ");
+
+        Matrix diagonalMatrix = new Matrix(n3);
+        Console.WriteLine("Диагонально заполненный массив:");
+        Console.WriteLine(diagonalMatrix);
+
+        // Задание 2: Вычисление и вывод сумм столбцов по заданию 2
+        int[] columnSums = chessMatrix.GetColumnSums();
+        Console.WriteLine("2. Суммы столбцов матрицы - шахматный массив (с учётом уменьшения количества элементов):");
+        for (int i = 0; i < columnSums.Length; i++)
+        {
+            Console.WriteLine($"Сумма столбца {i + 1}: {columnSums[i]}");
+        }
+        int maxColumnSum = chessMatrix.MaxColumnSum();
+        Console.WriteLine($"Максимальная сумма среди всех сумм: {maxColumnSum}");
+
+        // Вычисление выражения (A + 4 * B) - C^T с новыми массивами
+        int n4 = Matrix.InputInteger("3. Введите размерность массивов для вычисления выражения (должна быть квадратная матрица n x n): ");
+
+        Console.WriteLine("Введите элементы для массива A:");
+        Matrix A = new Matrix(n4, n4);
+
+        Console.WriteLine("Введите элементы для массива B:");
+        Matrix B = new Matrix(n4, n4);
+
+        Console.WriteLine("Введите элементы для массива C:");
+        Matrix C = new Matrix(n4, n4);
+
+        Matrix result = Matrix.CalculateExpression(A, B, C);
+        Console.WriteLine("Результат выражения (A + 4 * B) - C^T:");
+        Console.WriteLine(result);
+    }
+}
